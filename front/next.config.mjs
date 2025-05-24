4510abab69c99c983d211618a8ea503f2a88baee
nag763 <loic.labeye@tutanota.de>
2025-05-24 15:40:50 +0200

    ci: add docker

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true, // Or your existing configurations
+    output: 'standalone',
+};
 
 export default nextConfig;

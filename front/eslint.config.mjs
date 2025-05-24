a46c037c8c4c018011921bfc0abe509c1cf73c81
nag763 <loic.labeye@tutanota.de>
2025-05-24 14:56:42 +0200

    Add pretty config

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -1,14 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  ...compat.config({
+    extends: [
+      'next',
+      'next/core-web-vitals',
+      'plugin:prettier/recommended', // Make sure this is last
+    ],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          // Your Prettier rules here, e.g.:
+          'endOfLine': 'auto',
+          'semi': true,
+          'singleQuote': true,
+          'trailingComma': 'all'
+        }
+      ]
+    },
+  }),
+  // Add any other specific configurations here if needed
+];

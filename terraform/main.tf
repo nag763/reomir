2b2c1592cabca1d1d1b97bb007b44b2b05179183
Loïc <55486724+nag763@users.noreply.github.com>
2025-05-26 19:34:32 +0200

    Revert "chore: rm google-beta"

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
   }
 }
 

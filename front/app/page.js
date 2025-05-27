cc7a691ad5f4d843dfbe23a7946575d4d5a171bf
nag763 <loic.labeye@tutanota.de>
2025-05-27 21:46:32 +0200

    feat: add title

--- front/app/page.js
+++ front/app/page.js
@@ -3,6 +3,11 @@ import FeaturesSection from '@/components/FeaturesSection';
 import CtaSection from '@/components/CtaSection';
 import PageFooter from '@/components/PageFooter';
 
+export const metadata = {
+  title: 'Reomir',
+  description: 'Welcome to our application! Explore features and get started.',
+};
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100 font-mono p-8 md:p-16">

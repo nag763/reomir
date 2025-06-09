19480bed46e6f45a3932f5119c3c748b2786450b
nag763 <loic.labeye@tutanota.de>
2025-06-09 10:58:34 +0200

    feat: rm prompt on login

--- front/app/api/auth/[...nextauth]/route.js
+++ front/app/api/auth/[...nextauth]/route.js
@@ -48,7 +48,6 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
-          prompt: 'consent',
           access_type: 'offline',
           response_type: 'code',
         },
